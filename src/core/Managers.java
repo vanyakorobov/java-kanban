470a70e9ec7b96cca62d8cf702ee38a5e208f89d
Ivan <vanya.korobov@yandex.ru>
2023-04-18 01:58:38 +0300

    "Внести правки в проект"

--- src/core/Managers.java
+++ src/core/Managers.java
@@ -1,13 +1,5 @@
 package core;
 
-
-import model.Epic;
-import model.Status;
-import model.SubTask;
-import model.Task;
-
-import java.util.ArrayList;
-
 public class Managers {
 
     public static TaskManager getDefault() {
@@ -20,102 +12,5 @@ public class Managers {
 
         return new InMemoryHistoryManager();
     }
-
-}
-
-abstract class DefaultTaskManager implements TaskManager {
-
-
-    @Override
-    public int createTask(Task task) {
-        return 0;
-    }
-
-    @Override
-    public ArrayList<Task> getTasks() {
-        return null;
-    }
-
-    @Override
-    public void updateTask(Task task) {
-
-    }
-
-    @Override
-    public void clearTasks() {
-
-    }
-
-    @Override
-    public Task removeTask(int id) {
-        return null;
-    }
-
-    @Override
-    public Task getTaskId(int id) {
-        return null;
-    }
-
-    @Override
-    public int createSubTask(SubTask subTask) {
-        return 0;
-    }
-
-    @Override
-    public ArrayList<SubTask> getSubtaskTasks() {
-        return null;
-    }
-
-    @Override
-    public void updateSubTask(SubTask subTask) {
-
-    }
-
-    @Override
-    public void clearSubTasks() {
-
-    }
-
-    @Override
-    public void removeSubTask(SubTask subTask) {
-
-    }
-
-    @Override
-    public Task getSubTaskId(int id) {
-        return null;
-    }
-
-    @Override
-    public int createEpic(Epic epic) {
-        return 0;
-    }
-
-    @Override
-    public Status updateEpic(Epic epic, int epicId) {
-        return null;
-    }
-
-    @Override
-    public ArrayList<Epic> getEpics() {
-        return null;
-    }
-
-    @Override
-    public void clearEpics() {
-
-    }
-
-    @Override
-    public void removeEpic(Epic epic) {
-
-    }
-
-    @Override
-    public Task getSubEpicId(int id) {
-        return null;
-    }
-
-    // здесь реализуем все методы, определенные в интерфейсе TaskManager
 
 }
